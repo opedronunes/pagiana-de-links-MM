7d8ecf09c62fdf55dd65fa528da3804ac7991887
Pedro Nunes <adm.pedronunes@hotmail.com>
2024-03-26 22:56:40 -0300

    first commit

---
+++ src/app/data/social.tsx
@@ -0,0 +1,37 @@
+export const rs = [
+    {
+        id: 0,
+        name: 'Whatsapp',
+        url: 'https://wa.me/+5561999661929',
+        svg: '/wp.svg',
+        alt: 'Icone do whatsapp'
+    },
+    {
+        id: 1,
+        name: 'Instagram',
+        url: 'https://www.instagram.com/mmlocacoes.df/',
+        svg: '/insta.svg',
+        alt: 'Icone do Instagram'
+    },
+    {
+        id: 2,
+        name: 'Email',
+        url: 'mailto:mmlocacoes.df@gmail.com?subject=Orçamento',
+        svg: '/mail.svg',
+        alt: 'Icone de email'
+    },
+    {
+        id: 4,
+        name: 'Localização',
+        url: 'https://maps.app.goo.gl/zszfaiXvJsVgM2gY9',
+        svg: '/map.svg',
+        alt: 'Icone de Localização'
+    },
+    {
+        id: 5,
+        name: 'Site',
+        url: 'https://www.mmlocacoesdf.com.br',
+        svg: '/globe.svg',
+        alt: 'Icone do globo de internet'
+    },
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { url } from "inspector";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -12,7 +13,14 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "bg-dots": "url('/dot.svg')"
       },
+      colors: {
+        'yellow-600': '#FFF112 ',
+        'red-600': '#EC3237',
+        'gray-50': '#F4F3F3',
+        'gray-600': '#424242'
+      }
     },
   },
   plugins: [],
